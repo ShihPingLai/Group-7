e172cef7d2d49aca4a80c2260c349974e5f69c67
ch3cho19941010 <38750350+ch3cho19941010@users.noreply.github.com>
2018-05-10 17:20:57 +0800

    Update stock prediction.py

--- stock prediction.py
+++ stock prediction.py
@@ -3,3 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import fix_yahoo_finance as yf
+yf.pdr_override()
+data = web.get_data_yahoo('NKE', start = "2017-04-11", end = "2018-04-11")
+data.to_csv('output_NKE.csv')
+data.head()
+
